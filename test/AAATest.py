a117239e3a5b109bef734c115b2c3a830a060a42
Paul Water <p.r.water@setbased.nl>
2016-07-25 14:20:16 +0200

    Updated .scrutinizer.yml.

--- test/AAATest.py
+++ test/AAATest.py
@@ -10,11 +10,24 @@ import unittest
 from cleo import CommandTester
 from pystratum.application.PyStratumApplication import PyStratumApplication
 
+from test.DataLayer import DataLayer
+
 
 class AAATest(unittest.TestCase):
     """
     This test must run before all other tests because this test loads the stored routines.
     """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test0(self):
+        """
+        Create tables.
+        """
+        DataLayer.connect('localhost', 'test', 'test', 'test', 'test', 5432)
+        with open('test/ddl/create_tables.sql') as file:
+            sql = file.read()
+
+        DataLayer.execute_none(sql)
 
     # ------------------------------------------------------------------------------------------------------------------
     def test1(self):
